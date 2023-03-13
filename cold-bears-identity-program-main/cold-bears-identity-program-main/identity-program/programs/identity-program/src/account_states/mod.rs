cbeb1b64e78a71f4b38f9ca453c4c0f47d1a394d
Cold Bears <123092976+coldbears@users.noreply.github.com>
2023-03-13 22:00:58 +0100

    Delete cold-bears-identity-program-main/cold-bears-identity-program-main/identity-program directory

--- cold-bears-identity-program-main/cold-bears-identity-program-main/identity-program/programs/identity-program/src/account_states/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod identity;
-pub mod identity_manager;
-pub mod proof_of_online;
-
-pub use identity::*;
-pub use identity_manager::*;
-pub use proof_of_online::*;
--- cold-bears-identity-program-main/cold-bears-identity-program-main/identity-program/programs/identity-program/src/constants.rs
+++
@@ -1,2 +0,0 @@
-pub const ADMIN_PUBKEY_STR: &str = "FpLbdDnS61viAgJtSXHPKBuvKYRFQ7yc6rzuaLSy1MTc";
-pub const TREASURY_PUBKEY_STR: &str = "4reqRtKEqXggb7xAFobTnPpAXtoSi5zEBK8WLL5qyn1B";
